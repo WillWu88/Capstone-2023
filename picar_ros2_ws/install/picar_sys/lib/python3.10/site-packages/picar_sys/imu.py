03a7aee70bfeea112399d0772723be65210728c6
Will Wu <williamwu8-8@outlook.com>
2023-02-23 15:58:19 -0600

    extended IMU node, work in progress

--- picar_ros2_ws/install/picar_sys/lib/python3.10/site-packages/picar_sys/imu.py
+++ picar_ros2_ws/install/picar_sys/lib/python3.10/site-packages/picar_sys/imu.py
@@ -1,23 +1,54 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from snesor_msgs.msg import Imu
+from std_msgs.msg import String, Header
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Vector3, Quaternion
+import drivers.imu_driver 
 
 class IMUPublisher(Node):
 
     def __init__(self):
         super().__init__('imu_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.imu = drivers.imu_driver.IMUDriver()
+        self.publisher_ = self.create_publisher(Imu, 'imu_raw', 10) # history depty of 10
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.msg_count = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+
+        self.imu.update()
+        msg = self.populate_message
+
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.get_logger().info('Publishing: "%d"' % self.msg_count)
+        self.msg_count += 1
+
+    def populate_message(self):
+        # put current imu data into msg
+
+        msg = Imu()
+
+        # header config
+        msg.header.stamp = self.get_clock.now().to_msg()
+        msg.header.frame_id = '' #empty for now
+
+        # keeping at 0 for now, estimator relevant
+        msg.orientation = Quaternion()
+        msg.orientation_covariance = [0] * 9
+        msg.angular_velocity_covariance = [0] * 9
+        msg.linear_acceleration_covariance = [0] * 9
+
+        # sensor data here
+        msg.angular_velocity.x = self.imu.data[self.imu.index_dict['gyro_x']] 
+        msg.angular_velocity.y = self.imu.data[self.imu.index_dict['gyro_y']] 
+        msg.angular_velocity.z = self.imu.data[self.imu.index_dict['gyro_z']] 
+
+        msg.linear_acceleration.x = self.imu.data[self.imu.index_dict['acc_x']] 
+        msg.linear_acceleration.y = self.imu.data[self.imu.index_dict['acc_y']] 
+        msg.linear_acceleration.z = self.imu.data[self.imu.index_dict['acc_z']] 
+
+        return msg
 
 
 def main(args=None):
--- picar_ros2_ws/src/picar_sys/setup.py
+++ picar_ros2_ws/src/picar_sys/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
+import os
 
 package_name = 'picar_sys'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, 'drivers'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
