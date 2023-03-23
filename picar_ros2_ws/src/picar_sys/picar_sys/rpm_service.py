2e5b624cfda05a448925e8ccd441b50c3b726011
willwu <willwu@willwu-desktop.com>
2023-03-23 16:21:01 -0500

    srv files working

--- picar_ros2_ws/src/picar_sys/picar_sys/rpm_service.py
+++ picar_ros2_ws/src/picar_sys/picar_sys/rpm_service.py
@@ -1,29 +1,31 @@
+from example_interfaces.srv import AddTwoInts
+
 import rclpy
 from rclpy.node import Node
 
-from example_interfaces.srv import AddTwoInts
 
-class RpmService(Node):
+class MinimalService(Node):
 
     def __init__(self):
-        super().__init__('rpm_service')
+        super().__init__('minimal_service')
         self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
 
     def add_two_ints_callback(self, request, response):
-        response.sum = request.a + request.b 
-        self.get_logger().info('Incoming request\n a: %d b: %d' % (request.a, request.b)) 
+        response.sum = request.a + request.b
+        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
 
         return response
+
 
 def main(args=None):
     rclpy.init(args=args)
 
-    rpm_service = RpmService()
+    minimal_service = MinimalService()
 
-    rclpy.spin(rpm_service)
+    rclpy.spin(minimal_service)
 
     rclpy.shutdown()
 
-if __name__== '__main__':
-    main()
 
+if __name__ == '__main__':
+    main()
