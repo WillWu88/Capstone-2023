f67188706e0339497672b1c2bacd5ed3135b4c5b
Will Wu <williamwu8-8@outlook.com>
2023-04-19 21:03:12 -0500

    KF update

--- picar_ros2_ws/src/picar_sys/picar_sys/kalman_node.py
+++ picar_ros2_ws/src/picar_sys/picar_sys/kalman_node.py
@@ -7,9 +7,9 @@ from tutorial_interfaces.msg import GPS, PoseSetpoint, RPM, VelSetpoint, XFilter
 from message_filters import TimeSynchronizer, Subscriber
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
+import drivers.sensor_noise
 
-class KalmanX(Node):
-
+class KalmanNode(Node):
 
     def __init__(self):
         super().__init__('kalman_filter_x')
@@ -27,6 +27,16 @@ class KalmanX(Node):
         self.kf_init(self.kf_x, self.f_s, np.array([0, 1]), rpm_cov, 
                      Q_discrete_white_noise(2, 1./self.f_s, var=imu_x_var))
 
+        self.kf_y = KalmanFilter(dim_x=2, dim_z=1)
+        self.kf_init(self.kf_y, self.f_s, np.array([0, 1]), gps_cov,
+                     Q_discrete_white_noise(2, 1./self.f_s, var=imu_y_var))
+
+        self.kf_psi = KalmanFilter(dim_x=1, dim_z=1)
+        self.kf_init(self.kf_psi, self.f_s, np.array([1]), )
+
+        # GPS fusion, expand in the future
+        self.gps_sub = self.create_subscription(GPS, 'gps_raw', self.gps_callback, 10)
+
     def kf_init(self, kf, f_s, custom_H, custom_R, custom_Q):
         '''default: dt double integrator F=ma process 
             initial covariance set to identity
@@ -40,12 +50,15 @@ class KalmanX(Node):
     def kf_update(self, imu_msg, rpm_msg):
         pass
 
+    def gps_callback(self):
+        self.get_logger().info('GPS Update Received')
+
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    kalman_x = KalmanX()
+    kalman_x = KalmanNode()
 
     rclpy.spin(kalman_x)
 
