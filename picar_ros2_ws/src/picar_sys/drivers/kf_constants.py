040c3464281fb969576593bf049e2c6b8a2cf570
Will Wu <williamwu8-8@outlook.com>
2023-04-26 19:07:15 -0500

    KF tuning organization

--- picar_ros2_ws/src/picar_sys/drivers/kf_constants.py
+++ picar_ros2_ws/src/picar_sys/drivers/kf_constants.py
@@ -7,8 +7,8 @@ Q_mult_x = 1.
 R_mult_x = 1.
 Q_mult_y = 1.0
 R_mult_y = 1.0
-imu_x_mult = 5000
-gps_x_mult = 0.01
+imu_x_mult = 2000
+gps_x_mult = 1
 ms_x_mult = 0.5
 
 imu_x_mean = 0.0264
@@ -18,9 +18,9 @@ imu_y_var = 1.711e-6
 ms_mean = 0.
 ms_var = 0.0092
 gps_x_mean = 0.
-gps_x_var = 1. * gps_x_mult
+gps_x_var = 4.350e-5 * gps_x_mult
 gps_y_mean = 0.
-gps_y_var = 1.
+gps_y_var = 1.174e-4
 tan_mean = 0.
 tan_var = 0.0038
 
