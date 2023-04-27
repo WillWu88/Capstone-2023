5b4bdfdbb8f203d85b24236f4da3332b60d0a7c1
Onja <93228001+OnjaK@users.noreply.github.com>
2023-04-27 01:34:11 -0500

    Added navigation points in drivers folder

---
+++ picar_ros2_ws/src/picar_sys/drivers/navigation_points.py
@@ -0,0 +1,35 @@
+# All navigation points:
+
+origin_lat_deg = 38.0
+origin_lat_min = 64.8446
+origin_long_deg = -90.0
+origin_long_min = 30.2296
+
+p1_lat_min = 64.8646
+p1_long_min = 30.2166
+
+# Turn 1
+
+p2_lat_min = 64.8664
+p2_long_min = 30.2071
+
+p3_lat_min = 64.8588
+p3_long_min = 30.1743
+
+# Turn 2
+
+p4_lat_min = 64.8471
+p2_long_min = 30.1831
+
+p5_lat_min = 64.8345
+p5_long_min = 30.1891
+
+# Turn 3
+
+p6_lat_min = 64.8346
+p6_long_min = 30.1935
+
+# Go to origin
+
+
+
